25102feb85dcf64365bb53134af425a6dd3f9c9d
xiao <646163921qq@.com>
2019-03-22 09:45:50 +0800

    3

--- js/modile.js
+++ js/modile.js
@@ -26,20 +26,26 @@ function add() {
     $("#right").click(function() {
         var text = $("#add-input")[0].textContent
         if (text != "") {
-            var node = $("#cont div").clone()[0]
-            node.childNodes[1].children[0].innerText = text;
+            var node = $("#cont div").clone(true)[0];
+            node.childNodes[1].children[0].children[0].innerText = text;
             node.childNodes[1].style.left = 0;
             touchDown(node.childNodes[1])
-            $("#cont").prepend(node);
+                // $("#cont").prepend(node);
+            var tar = document.getElementsByClassName("item")[0];
+            var parent = tar.parentNode
+            parent.insertBefore(node, tar.nextSibling);
             $("#add-input")[0].textContent = "";
         }
         $("#add-background").hide();
     })
 }
 //用js原生写的移动端触屏事件
-function touchDown(div) {
-    var width = document.body.clientWidth * 0.15;
-    div.addEventListener('touchstart', function(event) {
+function touchDown(v) {
+    var width = document.body.clientWidth * 0.25;
+    v.childNodes[3].addEventListener('click', () => {
+        v.parentNode.parentNode.removeChild(v.parentNode);
+    })
+    v.childNodes[1].addEventListener('touchstart', function(event) {
         event.stopPropagation();
         event.preventDefault();
         var beginX = event.targetTouches[0].screenX;
@@ -54,13 +60,16 @@ function touchDown(div) {
             event.stopPropagation();
             event.preventDefault();
             endX = event.targetTouches[0].screenX;
-            scroll = endX - beginX + parseInt(window.getComputedStyle(div)['top'])
-            if (scroll >= 0) {
-                div.style.left = 0 + "px"
-            } else if (scroll >= -width) {
-                div.style.left = scroll + "px"
-            } else if (scroll < -width) {
-                div.style.left = -width + "px"
+            var vTop = parseInt(window.getComputedStyle(v)['top']);
+            scroll = endX - beginX + vTop;
+            if (((endX - beginX) < -10 || (endX - beginX) > 10)) {
+                if (scroll >= 0) {
+                    v.style.left = 0 + "px"
+                } else if (scroll >= -width) {
+                    v.style.left = scroll + "px"
+                } else if (scroll < -width) {
+                    v.style.left = -width + "px"
+                }
             }
         }
 
@@ -72,14 +81,24 @@ function touchDown(div) {
                 document.removeEventListener("touchmove", moveHandler, true);
 
             }
+            if ((endX - beginX) > -10 && (endX - beginX) < 10) {
+                done(v);
+            }
+            if (scroll >= -width / 2) {
+                v.style.left = 0 + "px"
+            } else if (scroll < -width / 2) {
+                v.style.left = -width + "px"
+            }
         }
     });
 }
 //这是用于完成待办时间的处理函数
-function done(e) {
-    if (e.children[0].style.opacity == "1") {
-        e.children[0].style.opacity = "0.5"
+function done(v) {
+    if (v.children[0].style.opacity == "1") {
+        v.children[0].style.opacity = "0.5"
+        v.children[0].children[0].style.textDecoration = "line-through"
     } else {
-        e.children[0].style.opacity = "1"
+        v.children[0].style.opacity = "1"
+        v.children[0].children[0].style.textDecoration = "none"
     }
 }
